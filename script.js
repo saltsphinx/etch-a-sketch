b40c3248f6ce6da5ab3249bb595c30826419e5fd
thomas <tremar.thomas19@gmail.com>
2021-05-27 04:25:11 -0500

    Added to JS program. Removes previous grid items when user changes grid size.

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const gridCanvas = document.querySelector('.canvas')
+const gridButton = document.querySelector('.sizeButton')
+
+gridButton.addEventListener('click', changeCanvasSize)
+
+function changeCanvasSize() {
+    let promptSize = prompt('What size would you like?', 16);
+    if (!isNaN(+promptSize)) {
+        setUpCanvas(+promptSize)
+    } else {
+        alert('Enter a number please.')
+    }
+}
 
 function setUpCanvas(num) {
     gridCanvas.style.setProperty('grid-template-columns', `repeat(${num}, auto)`)
     gridCanvas.style.setProperty('grid-template-rows', `repeat(${num}, auto)`)
-
+    checkChildren()
     createRow(num * num)
+}
+
+function checkChildren() {
+    if (gridCanvas.hasChildNodes()) {
+        for (; gridCanvas.childNodes.length > 0;) {
+            gridCanvas.firstChild.remove()
+        }
+    }
 }
 
 function createRow(num) {
@@ -12,6 +32,5 @@ function createRow(num) {
         const row = document.createElement('div')
         gridCanvas.appendChild(row)
         row.classList.add('item')
-        console.log(i)
     }
 }
