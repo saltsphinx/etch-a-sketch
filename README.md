a5ffa558f44e6b83d9720ed6147900d13c9dacbb
thomas <tremar.thomas19@gmail.com>
2021-06-01 10:16:12 -0500

    Project completed. User is able to paint over canvas, and each pass of a square gives it a random color. Added to README.md

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # etch-a-sketch
+
+A grid painting program that allows the user to paint over squares, and change the grid size.
+
+Simple : Write a program that allows the user to draw over a grid, and allows them to change the grid size.
+
+Does your program have a user interface? What will it look like? What functionality will the interface have? Sketch this out on paper.
+Yes, there is a user interface. The interface will be simple, only the painting grid and a button and prompt that allows changing of the grid size.
+
+What inputs will your program have? Will the user enter data or will you get input from somewhere else?
+The inputs will come from the grid size prompt, and user's mouse. Inputs will come directly from user.
+
+What’s the desired output?
+From the prompt, a size in the grid size. From the user's mouse, a color change of the grid squares.
+
+Given your inputs, what are the steps necessary to return the desired output?
+
+Add an eventlistner to a button element that calls a function,
+Function prompts user to enter a number between 1 and 32,
+check if prompt value is a number and that its within the asked range,
+call function with prompt value as parameter that sets up grid and removes previous divs if they exist,
+create css styles for grid canvas with display set to grid,
+in function that sets up canvas, set property of both rows and columns to 'repeat' with value of prompt,
+create function that takes the propmt value squared and creates divs inside of canvas with div style applied to each in for loop,
+create a for loop that adds event listners to divs in canvas that trigger function when hovered over,
+create function that creates random hexa code for color, and pass back to event listner
--- script.js
+++ script.js
@@ -1,36 +1,65 @@
-const gridCanvas = document.querySelector('.canvas')
-const gridButton = document.querySelector('.sizeButton')
+const gridCanvas = document.querySelector('.canvas');
+const gridButton = document.querySelector('.sizeButton');
 
-gridButton.addEventListener('click', changeCanvasSize)
+gridButton.addEventListener('click', changeCanvasSize);
+
+setUpCanvas(16);
 
 function changeCanvasSize() {
     let promptSize = prompt('What size would you like?', 16);
     if (!isNaN(+promptSize)) {
-        setUpCanvas(+promptSize)
+        if (+promptSize >= 1 && +promptSize <= 32) {
+            setUpCanvas(+promptSize);
+        } else {
+            alert('Please enter a number between 1 and 32.');
+        }
     } else {
-        alert('Enter a number please.')
+        alert('Enter a number please.');
     }
 }
 
 function setUpCanvas(num) {
-    gridCanvas.style.setProperty('grid-template-columns', `repeat(${num}, auto)`)
-    gridCanvas.style.setProperty('grid-template-rows', `repeat(${num}, auto)`)
-    checkChildren()
-    createRow(num * num)
+    gridCanvas.style.setProperty('grid-template-columns', `repeat(${num}, auto)`);
+    gridCanvas.style.setProperty('grid-template-rows', `repeat(${num}, auto)`);
+    checkChildren();
+    createRow(num * num);
+    addHoverEvent();
 }
 
 function checkChildren() {
     if (gridCanvas.hasChildNodes()) {
         for (; gridCanvas.childNodes.length > 0;) {
-            gridCanvas.firstChild.remove()
+            gridCanvas.firstChild.remove();
         }
     }
 }
 
 function createRow(num) {
     for (let i = 0; i < num; i++) {
-        const row = document.createElement('div')
-        gridCanvas.appendChild(row)
-        row.classList.add('item')
+        const row = document.createElement('div');
+        gridCanvas.appendChild(row);
+        row.classList.add('item');
+    }
+}
+
+function generateRandomColor() {
+    let hex = '0123456789abcdef'
+    let returnValue = '#';
+    for (let i = 0; i < 6; i++) {
+        const randomNumber = Math.floor(Math.random()*17)
+        returnValue += hex.substr(randomNumber, 1)
+    }
+    return returnValue
+}
+
+function addHoverEvent() {
+    let canvasChildren = gridCanvas.childNodes
+    for (let i = 0; i < canvasChildren.length; i++) {
+        canvasChildren[i].addEventListener('mouseover', function (e) {
+            let childDiv = e.target
+            let divStyle = childDiv.style
+            divStyle.setProperty('background-color', `${generateRandomColor()}`)
+             
+        })
     }
 }
